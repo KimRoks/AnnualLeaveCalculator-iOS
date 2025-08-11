f32a935861ff9b16f48b3f7dd88c6acecc492a62
KimRoks <kimroks3161@gmail.com>
2025-08-11 11:39:31 +0900

    Initial Commit

---
+++ AnnualLeaveCalculator/AnnualLeaveCalculator/Presentaion/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AnnualLeaveCalculator
+//
+//  Created by 김경록 on 8/11/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
