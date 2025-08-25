eb749602f0ef8cfb3d79f71f26b8386669ed2f24
KimRoks <kimroks3161@gmail.com>
2025-08-25 17:54:21 +0900

    refactor: rootVC 수정, CustomNaviVC 작성

--- AnnualLeaveCalculator/AnnualLeaveCalculator/App/SceneDelegate.swift
+++ AnnualLeaveCalculator/AnnualLeaveCalculator/App/SceneDelegate.swift
@@ -15,9 +15,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = MainViewController()
+        let navigationController = LawdingNavigationController(rootViewController: rootViewController)
+    
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ AnnualLeaveCalculator/AnnualLeaveCalculator/Presentaion/LawdingNavigationController.swift
@@ -0,0 +1,70 @@
+//
+//  LawdingNavigationController.swift
+//  AnnualLeaveCalculator
+//
+//  Created by 김경록 on 8/11/25.
+//
+import UIKit
+
+final class LawdingNavigationController: UINavigationController {
+
+    // 공용 왼쪽 타이틀 Label
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lawding"
+        label.font = .pretendard(style: .bold, size: 20)
+        label.textColor = UIColor(hex: "0015FF")
+        return label
+    }()
+
+    // 공용 오른쪽 버튼
+    private let infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let image = UIImage(systemName: "info.circle.fill")
+        button.setImage(image, for: .normal)
+        button.tintColor = UIColor(hex: "0015FF")
+        button.addTarget(nil, action: #selector(infoButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc private func infoButtonTapped() {
+        print("오른쪽 버튼 탭")
+    }
+
+    private func setupCommonNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(hex: "#F5F5F5")
+        appearance.shadowColor = .clear
+        navigationBar.standardAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.isTranslucent = false
+    }
+
+    private func applyNavigationItems(to viewController: UIViewController) {
+        viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+        viewController.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: infoButton)
+    }
+}
+
+extension LawdingNavigationController: UINavigationControllerDelegate {
+    func navigationController(
+        _ navigationController: UINavigationController,
+        willShow viewController: UIViewController,
+        animated: Bool
+    ) {
+        setupCommonNavigationBar()
+        applyNavigationItems(to: viewController)
+    }
+}
