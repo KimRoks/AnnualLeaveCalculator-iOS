33680c82222244431df5615c8f3870f7fd577166
KimRoks <kimroks3161@gmail.com>
2025-12-03 16:38:23 +0900

    refactor: BaseVC 배경색 변경

--- AnnualLeaveCalculator/AnnualLeaveCalculator/Presentaion/Common/ViewComponents/BaseViewController.swift
+++ AnnualLeaveCalculator/AnnualLeaveCalculator/Presentaion/Common/ViewComponents/BaseViewController.swift
@@ -11,7 +11,7 @@ class BaseViewController: UIViewController, NavigationBarTitlePresentable, Toast
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(hex: "#F5F5F5")
+        view.backgroundColor = UIColor(hex: "#FBFBFB")
         hookInteractivePopGesture()
     }
 
